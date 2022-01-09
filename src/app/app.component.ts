05fdfa27c814850c1c94b917fa7f0b64742a4c4e
Rafael Marinho <rafael.mgf2001@gmail.com>
2022-01-09 19:06:39 -0300

    criando rota para home & componente home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'team-manager';
+  teste = true;
+
+  lista = [{nome: 'Nome 1'}, {nome: 'Nome 2'}, {nome: 'Nome 3'}];
 }
